3b98a265385c3fb591fe99c5bd08cca85d1a546b
ngochung.dev <tranngochung.dev@gmail.com>
2026-02-23 23:39:55 +0700

    initial database

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      username: process.env.POSTGRES_USER || 'admin',
+      password: process.env.POSTGRES_PASSWORD || '123456',
+      database: process.env.POSTGRES_DB || 'realestate',
+      autoLoadEntities: true,
+      synchronize: true, // dev only
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
